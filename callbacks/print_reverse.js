67b5d1383c04dcb6d4cfd23bd7731420e9e561ff
paulabarreto <paulavilaca@gmail.com>
2018-09-03 16:33:21 +0000

    reverse

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,1 +1,15 @@
 var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/reverse.html'
+};
+
+function printReverse (html) {
+  html = html.split("");
+  html = html.reverse();
+  html = html.join("");
+  console.log(html);
+}
+
+console.log(getHTML(requestOptions, printReverse));
