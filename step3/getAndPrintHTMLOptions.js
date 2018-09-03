acb81c513a789e4d1b94ca5e2249fa08e7ccf8df
paulabarreto <paulavilaca@gmail.com>
2018-09-03 15:42:23 +0000

    step3

---
+++ step3/getAndPrintHTMLOptions.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+var printData = [];
+
+
+function getAndPrintHTML (options) {
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+    printData.push(data);
+    console.log(printData);
+
+  });
+});
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+console.log(getAndPrintHTML(requestOptions));
