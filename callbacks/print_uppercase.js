8743ee5d604f29899b2e6cf6538f83df06efbad0
paulabarreto <paulavilaca@gmail.com>
2018-09-03 16:25:04 +0000

    uppercase fixed

--- callbacks/print_uppercase.js
+++ callbacks/print_uppercase.js
@@ -2,12 +2,12 @@ var getHTML = require('./http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step5.html'
+  path: '/http-examples/step6/uppercase.html'
 };
 
-function printHTML (html) {
+function printUpperCase (html) {
   html = html.toUpperCase();
   console.log(html);
 }
 
-console.log(getHTML(requestOptions, printHTML));
+console.log(getHTML(requestOptions, printUpperCase));
